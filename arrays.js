d799cb0af84d2358855a3470d3d233b4ee3ffd4a
lukefrederick <lfrederick220@gmail.com>
2025-07-18 19:34:17 -0500

    Submission

---
+++ arrays.js
@@ -0,0 +1,10 @@
+let groceryItems = ["Apples", "Eggs", "Milk", "Crackers", "Lettuce"];
+let prices = [2.99, 4.50, 3.00];
+
+console.log(groceryItems);
+console.log(prices);
+console.log(groceryItems[1], groceryItems[2], groceryItems[4]);
+console.log(prices[1], prices[0]);
+
+let names = ["Alex", "Luke", "Will"];
+console.log(names);
